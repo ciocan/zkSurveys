0f386fa017597d4ef4b54ffaf1b4ef29af947c5c
Radu Ciocan <ciocan@gmail.com>
2023-10-26 09:33:33 +0100

    remove next defaults

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    printWidth: 120,
+    tabWidth: 2,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    semi: true,
+    useTabs: false,
+    proseWrap: 'preserve',
+};
